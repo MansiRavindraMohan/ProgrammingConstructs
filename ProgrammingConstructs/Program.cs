60c973f30d62dbb504f4feb3782aa706d7e07985
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-27 16:56:00 +0530

    Program for checking two integers are equal or not

---
+++ ProgrammingConstructs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgrammingConstructs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Number number= new Number();
+            number.Compare();
+        }
+    }
+}
