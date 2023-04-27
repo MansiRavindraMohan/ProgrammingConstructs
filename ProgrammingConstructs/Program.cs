21a25ff3e03bd8b2634b0faff77553ca4ee9e6e4
MansiRavindraMohan <manasimohan29@gmail.com>
2023-04-27 18:26:24 +0530

    Program for performing Flip Coin problem till either Heads or Tails wins 20 times

--- ProgrammingConstructs/Program.cs
+++ ProgrammingConstructs/Program.cs
@@ -4,8 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            FactorialOfNumber factorialOfNumber = new FactorialOfNumber();
-            factorialOfNumber.FactorialOfNo();
+            FlipCoin flipCoin= new FlipCoin();
+            flipCoin.FlipCoinMethod();
             Console.ReadKey();
         }
     }
